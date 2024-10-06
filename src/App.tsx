f7dce33135ef7c8fb60921f9c72e0fb390eca3e9
Duy Nguyễn <nguyenduy30234@gmail.com>
2024-10-06 09:56:43 +0700

    feat: udpate title calendar

--- src/App.tsx
+++ src/App.tsx
@@ -2,46 +2,18 @@ import "./index.scss";
 import { useRoutes } from "react-router-dom";
 import { routes } from "@/routes";
 import { Suspense } from "react";
+import { Helmet } from "react-helmet";
 function App() {
   const element = useRoutes(routes);
-
-  // const eventList = [
-  //   {
-  //     eventDate: "2024-10-22",
-  //     events: [
-  //       {
-  //         title: "First session with Alex Stan",
-  //         timeStart: "2024-10-22T06:00:00Z",
-  //         timeEnd: "2024-10-22T23:30:00Z",
-  //         type: 1, //client
-  //       },
-  //       {
-  //         title: "Color Fiesta season 13: PARANORMAL",
-  //         timeStart: "2024-10-22T08:00:00Z",
-  //         timeEnd: "2024-10-22T08:00:00Z",
-  //         type: 2, //webinar
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     eventDate: "2024-10-04",
-  //     events: [
-  //       {
-  //         title: "MANRIDE UNCHARTED - VIETNAM",
-  //         timeStart: "2024-10-04T08:00:00Z",
-  //         timeEnd: "2024-10-04T12:00:00Z",
-  //         type: 1,
-  //       },
-  //       {
-  //         title: "Family Office: From Theory to Reality",
-  //         timeStart: "2024-10-04T6:00:00Z",
-  //         timeEnd: "2024-10-04T09:00:00Z",
-  //         type: 2,
-  //       },
-  //     ],
-  //   },
-  // ];
-  return <Suspense fallback={<>Loading</>}>{element}</Suspense>;
+  return (
+    <Suspense fallback={<>...Loading</>}>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Calendar</title>
+      </Helmet>
+      {element}
+    </Suspense>
+  );
 }
 
 export default App;
